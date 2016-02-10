590d08573938db7392a9844ad253530f2e19c709
Alex <amhughes@g.cofc.edu>
2016-02-10 11:17:26 -0500

    added read/write json

--- src/Person.java
+++ src/Person.java
@@ -7,8 +7,11 @@ public class Person {
     public boolean isAlive = true;
    // public static StreetAddress address;
 
+    public Person() {
+    }
 
-       // would allow the option to create a blank empty object
+
+    // would allow the option to create a blank empty object
 
    /* public Person(String name, int age) {
         this(name, age, true);   calling next constructor to fill name and age and explicitly setting isAlive to true
