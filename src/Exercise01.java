3e779967fb310e3595e4bfbf19bb6891fcc5ee13
Alex <amhughes@g.cofc.edu>
2016-02-11 10:22:42 -0500

    exercise01 added

---
+++ src/Exercise01.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by alexanderhughes on 2/11/16.
+ */
+public class Exercise01 {
+
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Bob", "David", "Edward", "Albert", "Beth"};
+        HashMap<Character, ArrayList<String>> nameMap = new HashMap<>();
+        //create empty ArrayLists
+        for (String name : names) {
+            char firstLetter = name.charAt(0);
+            if (!nameMap.containsKey(firstLetter)) {
+                nameMap.put(firstLetter, new ArrayList<>());
+            }
+            else {
+                nameMap.get(firstLetter).add(name);
+            }
+        }
+        //add names
+       // for (String name : names) {
+       //     char firstLetter = name.charAt(0);
+       //     nameMap.get(firstLetter).add(name);
+      //  }
+        System.out.println(nameMap);
+
+    }
+}
+
+
+
+
+
+
+
+/*   HashMap<String, ArrayList<String>> nameMap = new HashMap<>();
+for (String name : names) {
+        String firstLetter = String.valueOf(name.charAt(0));
+        if (String.)
+        }
+        */
